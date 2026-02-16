20e1a3ab807958f8e29f184a958075b38391ef4d
Mar-proyectos <emprproyect@gmail.com>
2026-02-15 19:34:43 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -218,7 +218,7 @@ export default function Portafolio() {
             playsInline
             className="w-full h-full object-cover opacity-60"
           >
-            <source src="/videos/galaxy.mp4" type="video/mp4" />
+           <source src="/mi-portafolio/videos/galaxy.mp4" type="video/mp4" />
           </video>
           <div className="absolute inset-0 bg-black/20"></div>
         </div>
